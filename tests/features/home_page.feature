d46365d3f2e7f70ca3fec991c952d6453226bb2b
zhannaderk <janeorlenko@gmail.com>
2025-01-17 19:54:27 +0200

    first commit

---
+++ tests/features/home_page.feature
@@ -0,0 +1,8 @@
+Feature: Home Page Title
+  As a user
+  I want to verify the title of the home page
+  So that I can ensure the page loads correctly
+
+  Scenario: Verify Home Page Title
+    Given the home page is open
+    Then the title should be correct
--- tests/test_home_page.py
+++ tests/test_home_page.py
@@ -1,12 +1,28 @@
-import pages.home_page
+import sys
+import os
+import pytest
+from pytest_bdd import scenarios, given, then
+from conftest import driver
+from pages.home_page import HomePage
 
 
-def test_home_page_title(driver):
-    # Створюємо об'єкт сторінки
-    home_page = pages.home_page.HomePage(driver)
+# Додаємо шлях до каталогу pages
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
-    # Відкриваємо сторінку
+
+scenarios('features/home_page.feature')
+
+
+@pytest.fixture
+def home_page(driver):
+    return HomePage(driver)
+
+
+@given('the home page is open')
+def open_home_page(home_page):
     home_page.open()
 
-    # Перевіряємо, чи правильний заголовок
+
+@then('the title should be correct')
+def verify_home_page_title(home_page):
     assert home_page.is_title_correct(), "Home Page title is incorrect"
